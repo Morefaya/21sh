45590f934b283cb50210aad81ccac1a4ae51ebee
Morefaya <jeancharles.cazako@gmail.com>
2016-11-24 01:31:01 +0100

    first commit

---
+++ include/21sh.h
@@ -0,0 +1,7 @@
+#ifndef 21SH_H
+# define 21SH_H
+
+# include "libft.h"
+# include "ft_printf.h"
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "21sh.h"
+
+int	main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+
+	return (0);
+}
